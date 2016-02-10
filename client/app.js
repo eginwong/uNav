a90ab6539ffcee374a7c52a7849783958649d313
eginwong <e.gin.wong@gmail.com>
2016-02-10 16:11:52 -0500

    Added support and about routing in SPA

--- client/app.js
+++ client/app.js
@@ -18,6 +18,16 @@ uNav.config(function($routeProvider) {
   .when('/nearyou', {
     templateUrl : 'app/partials/nearyou.html',
     controller  : 'nearyouController'
+  })
+
+  .when('/about', {
+    templateUrl : 'app/partials/about.html',
+    controller  : 'aboutController'
+  })
+
+  .when('/support', {
+    templateUrl : 'app/partials/support.html',
+    controller  : 'supportController'
   });
 });
 
@@ -25,7 +35,6 @@ uNav.config(function($routeProvider) {
 uNav.controller('mainController', function($scope) {
   // create a message to display in our view
   $scope.message = 'home';
-
 });
 
 uNav.controller('searchController', function($scope) {
@@ -34,4 +43,12 @@ uNav.controller('searchController', function($scope) {
 
 uNav.controller('nearyouController', function($scope) {
   $scope.message = 'nearyou';
+});
+
+uNav.controller('aboutController', function($scope) {
+  $scope.message = 'about';
+});
+
+uNav.controller('supportController', function($scope) {
+  $scope.message = 'support';
 });
