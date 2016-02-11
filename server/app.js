b4268fbb40eaad26b451b550814e45e561377061
eginwong <e.gin.wong@gmail.com>
2016-02-11 10:26:05 -0500

    fix code by moving a variable up.

--- server/app.js
+++ server/app.js
@@ -1,47 +1,17 @@
 var express = require('express');
+var app = express();
 var server = require('http').Server(app);
 var bodyParser = require('body-parser');
 
-var app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
-app.use(express.static('client'));
+var router = express.Router();              // get an instance of the express Router
 
-app.get('/*', function(req, res, next){
-  var url = req.url;
-  console.log('/GET', req.url);
-  //Checking for urls like ../../passwd etc
-  if(!url.match(/\.\.+?\//)){
-    res.sendFile(req.url, {root:'./client'});
-  } else if(url === '/'){
-    next();
-  } else {
-    res.status(405).send('Did you try something naughty?');
-  }
+// test route to make sure everything is working (accessed at GET http://localhost:8080/api)
+router.get('/', function(req, res) {
+  res.json({ message: 'hooray! welcome to our api!' });
 });
-
-app.get('/', function(req, res){
-  res.sendFile('index.html', {root:'./client'});
-});
-
-//Config checks process.env, otherwise sets to 9000
-var port = process.env.PORT || 9000;
-
-module.exports.listen = function(){
-  server.listen(port, function(){
-    console.log('Server listening on port', port);
-  })
-};
-
-
-// app.use(bodyParser.urlencoded({ extended: true }));
-// app.use(bodyParser.json());
-
-// var router = express.Router();              // get an instance of the express Router
-//
-// // test route to make sure everything is working (accessed at GET http://localhost:8080/api)
-// router.get('/', function(req, res) {
-//   res.json({ message: 'hooray! welcome to our api!' });
-// });
 
 // router.route('/buildings')
 
@@ -85,4 +55,32 @@ module.exports.listen = function(){
 
 // REGISTER OUR ROUTES -------------------------------
 // all of our routes will be prefixed with /api
-// app.use('/api', router);
+app.use('/api', router);
+
+app.use(express.static('client'));
+
+app.get('/*', function(req, res, next){
+  var url = req.url;
+  console.log('/GET', req.url);
+  //Checking for urls like ../../passwd etc
+  if(!url.match(/\.\.+?\//)){
+    res.sendFile(req.url, {root:'./client'});
+  } else if(url === '/'){
+    next();
+  } else {
+    res.status(405).send('Did you try something naughty?');
+  }
+});
+
+app.get('/', function(req, res){
+  res.sendFile('index.html', {root:'./client'});
+});
+
+//Config checks process.env, otherwise sets to 9000
+var port = process.env.PORT || 9000;
+
+module.exports.listen = function(){
+  server.listen(port, function(){
+    console.log('Server listening on port', port);
+  })
+};
