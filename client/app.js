91326681db234ab7f702e44793dbdb06b7a3de28
eginwong <e.gin.wong@gmail.com>
2016-02-20 18:03:53 -0500

    Work in progress.

--- client/app.js
+++ client/app.js
@@ -35,6 +35,18 @@ uNav.service('sharedProperties', function() {
   }
 });
 
+uNav.directive('chosen', function($timeout) {
+
+  return {
+    restrict: 'A',
+    link: function(scope, element, attr) {
+        $timeout(function () {
+          element.chosen();
+        }, 200, false);
+      }
+    }
+});
+
 // create the controller and inject Angular's $scope
 uNav.controller('mainController', function($scope) {
   // create a message to display in our view
@@ -81,82 +93,81 @@ uNav.controller('navController', function($scope, $resource, sharedProperties, u
   }
 
   uiGmapGoogleMapApi.then(function (maps) {
-          $scope.googlemap = {};
-          $scope.map = {
-              center: {
-                  latitude: 43.47035091238624,
-                  longitude: -80.54049253463745
-              },
-              zoom: 20,
-              pan: 1,
-              options: $scope.mapOptions,
-              control: {},
-              events: {
-                  tilesloaded: function (maps, eventName, args) {},
-                  dragend: function (maps, eventName, args) {},
-                  zoom_changed: function (maps, eventName, args) {}
-              }
-          };
-      });
-
-      $scope.windowOptions = {
-          show: false
-      };
-
-      $scope.onClick = function (data) {
-          $scope.windowOptions.show = !$scope.windowOptions.show;
-          console.log('$scope.windowOptions.show: ', $scope.windowOptions.show);
-          console.log('This is a ' + data);
-          //alert('This is a ' + data);
-      };
-
-      $scope.closeClick = function () {
-          $scope.windowOptions.show = false;
-      };
-
-      $scope.title = "Window Title!";
-
-      uiGmapIsReady.promise() // if no value is put in promise() it defaults to promise(1)
-      .then(function (instances) {
-          console.log(instances[0].map); // get the current map
-      })
-          .then(function () {
-          $scope.addMarkerClickFunction($scope.markers);
-      });
-
-      $scope.markers = [{
-          id: 0,
-          coords: {
-              latitude: 37.7749295,
-              longitude: -122.4194155
-          },
-          data: 'restaurant'
-      }, {
-          id: 1,
-          coords: {
-              latitude: 37.79,
-              longitude: -122.42
-          },
-          data: 'house'
-      }, {
-          id: 2,
-          coords: {
-              latitude: 37.77,
-              longitude: -122.41
-          },
-          data: 'hotel'
-      }];
-
-      $scope.addMarkerClickFunction = function (markersArray) {
-          angular.forEach(markersArray, function (value, key) {
-              value.onClick = function () {
-                  $scope.onClick(value.data);
-                  $scope.MapOptions.markers.selected = value;
-              };
-          });
-      };
-
-
+  })
+      //     $scope.googlemap = {};
+      //     $scope.map = {
+      //         center: {
+      //             latitude: 43.47035091238624,
+      //             longitude: -80.54049253463745
+      //         },
+      //         zoom: 20,
+      //         pan: 1,
+      //         options: $scope.mapOptions,
+      //         control: {},
+      //         events: {
+      //             tilesloaded: function (maps, eventName, args) {},
+      //             dragend: function (maps, eventName, args) {},
+      //             zoom_changed: function (maps, eventName, args) {}
+      //         }
+      //     };
+      // });
+      //
+      // $scope.windowOptions = {
+      //     show: false
+      // };
+      //
+      // $scope.onClick = function (data) {
+      //     $scope.windowOptions.show = !$scope.windowOptions.show;
+      //     console.log('$scope.windowOptions.show: ', $scope.windowOptions.show);
+      //     console.log('This is a ' + data);
+      //     //alert('This is a ' + data);
+      // };
+      //
+      // $scope.closeClick = function () {
+      //     $scope.windowOptions.show = false;
+      // };
+      // $scope.title = "Window Title!";
+      //
+      // uiGmapIsReady.promise() // if no value is put in promise() it defaults to promise(1)
+      // .then(function (instances) {
+      //     console.log(instances[0].map); // get the current map
+      // })
+      //     .then(function () {
+      //     $scope.addMarkerClickFunction($scope.markers);
+      // });
+      //
+      // $scope.markers = [{
+      //     id: 0,
+      //     coords: {
+      //         latitude: 37.7749295,
+      //         longitude: -122.4194155
+      //     },
+      //     data: 'restaurant'
+      // }, {
+      //     id: 1,
+      //     coords: {
+      //         latitude: 37.79,
+      //         longitude: -122.42
+      //     },
+      //     data: 'house'
+      // }, {
+      //     id: 2,
+      //     coords: {
+      //         latitude: 37.77,
+      //         longitude: -122.41
+      //     },
+      //     data: 'hotel'
+      // }];
+      //
+      // $scope.addMarkerClickFunction = function (markersArray) {
+      //     angular.forEach(markersArray, function (value, key) {
+      //         value.onClick = function () {
+      //             $scope.onClick(value.data);
+      //             $scope.MapOptions.markers.selected = value;
+      //         };
+      //     });
+      // };
+      //
       // $scope.MapOptions = {
       //     minZoom: 3,
       //     zoomControl: false,
@@ -170,34 +181,24 @@ uNav.controller('navController', function($scope, $resource, sharedProperties, u
       //     markers: {
       //         selected: {}
       //     },
-      //     {styles: [{
-      //       stylers: [{
-      //         hue: "#00ffe6" },{ saturation: -20 }]
-      //       },{featureType: "road", elementType: "geometry", stylers: [{ lightness: 100 },{ visibility: "simplified" }]},{featureType: "road", elementType: "labels", stylers: [ { visibility: "off" } ]}]}
       //     styles: [{
-      //         featureType: "poi",
-      //         elementType: "labels",
-      //         stylers: [{
-      //             visibility: "off"
-      //         }]
-      //     }, {
-      //         featureType: "transit",
-      //         elementType: "all",
-      //         stylers: [{
-      //             visibility: "off"
-      //         }]
-      //     }],
+      //           stylers: [
+      //             { hue: "#00ffe6" },
+      //             { saturation: -20 }
+      //           ]
+      //         },{
+      //           featureType: "road",
+      //           elementType: "geometry",
+      //           stylers: [
+      //             { lightness: 100 },
+      //             { visibility: "simplified" }
+      //           ]
+      //         },{
+      //           featureType: "road",
+      //           elementType: "labels",
+      //           stylers: [
+      //             { visibility: "off" }
+      //           ]
+      //       }]
       // };
-});
-
-uNav.directive('chosen', function($timeout) {
-
-  return {
-    restrict: 'A',
-    link: function(scope, element, attr) {
-        $timeout(function () {
-          element.chosen();
-        }, 200, false);
-      }
-    }
 });
